08bea009462cc40039e0449ad8265cc1cbf2e52a
KingsleyEE <kingejiofor9@gmail.com>
2023-02-24 04:44:27 -0800

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_digonal(0);
+    print_digonal(2);
+    print_digonal(10);
+    print_digonal(-4);
+    return (0);
+}
