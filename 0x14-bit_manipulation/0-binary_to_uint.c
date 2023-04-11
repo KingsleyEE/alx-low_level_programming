7ad06a45946cce1290ad78daaf1ad980c8373813
KingsleyEE <kingejiofor9@gmail.com>
2023-04-11 07:38:16 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
 unsigned int binary_to_unit(const char *b)
 {
 	int i;
-	unsigned int decval = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
@@ -17,7 +17,7 @@ unsigned int binary_to_unit(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		decval = 2 * decval + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (decval);
+	return (dec_val);
 }
